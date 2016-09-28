b05799d0f06d890bae920a9fa39907c62a99b64c
Taro Era <era@crypton.co.jp>
2016-09-28 13:42:28 +0900

    バージョン情報追加

--- SapporoGo/SapporoGo/SettingTableViewController.swift
+++ SapporoGo/SapporoGo/SettingTableViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class SettingTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
-
+    
     @IBOutlet weak var tableView: UITableView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,18 +20,29 @@ class SettingTableViewController: UIViewController, UITableViewDataSource, UITab
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("settingCell")
-        cell?.textLabel?.text = "ライセンス情報"
-        
-        let image:UIImage!
-        image = UIImage(named:"ic_copyright")
-        cell!.imageView!.image = image.resize(CGSizeMake(20, 20))
-        return cell!
+        if indexPath.row == 0{
+            let cell = tableView.dequeueReusableCellWithIdentifier("settingCell")
+            cell?.textLabel?.text = "ライセンス情報"
+            let image:UIImage!
+            image = UIImage(named:"ic_copyright")
+            cell!.imageView!.image = image.resize(CGSizeMake(20, 20))
+            return cell!
+        }
+        else{
+            let cell = tableView.dequeueReusableCellWithIdentifier("versionCell")
+            cell?.textLabel?.text = "バージョン"
+            let image:UIImage!
+            cell?.detailTextLabel?.text = "1.0.0(6)"
+            image = UIImage(named:"ic_info")
+            cell!.imageView!.image = image.resize(CGSizeMake(20, 20))
+            return cell!
+        }
+
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
