1a78336abb0e871fe35e9af63d0f4a84006b97a8
Taro Era <era@crypton.co.jp>
2016-09-28 14:24:51 +0900

    ユーザー名の変更

--- SapporoGo/SapporoGo/SettingTableViewController.swift
+++ SapporoGo/SapporoGo/SettingTableViewController.swift
@@ -20,12 +20,20 @@ class SettingTableViewController: UIViewController, UITableViewDataSource, UITab
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 3
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
         if indexPath.row == 0{
+            let cell = tableView.dequeueReusableCellWithIdentifier("userNameCell")
+            cell?.textLabel?.text = "ユーザー名の変更"
+            cell?.detailTextLabel?.text = UserDefaultSurpport.userName
+            let image:UIImage!
+            image = UIImage(named:"ic_user")
+            cell!.imageView!.image = image.resize(CGSizeMake(20, 20))
+            return cell!
+        }
+        else if indexPath.row == 1{
             let cell = tableView.dequeueReusableCellWithIdentifier("settingCell")
             cell?.textLabel?.text = "ライセンス情報"
             let image:UIImage!
@@ -42,10 +50,49 @@ class SettingTableViewController: UIViewController, UITableViewDataSource, UITab
             cell!.imageView!.image = image.resize(CGSizeMake(20, 20))
             return cell!
         }
-
+        
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        if indexPath.row == 0{
+            let alert:UIAlertController = UIAlertController(title:"ユーザー名の変更",
+                                                            message: "ユーザー名を入力してください",
+                                                            preferredStyle: UIAlertControllerStyle.Alert)
+            
+            //textfiledの追加
+            alert.addTextFieldWithConfigurationHandler({(text:UITextField!) -> Void in})
+            
+            let textFields:Array<UITextField>? =  alert.textFields as Array<UITextField>?
+            if textFields != nil {
+                for textField:UITextField in textFields! {
+                    //各textにアクセス
+                    textField.text = UserDefaultSurpport.userName
+                }
+            }
+            
+            let defaultAction:UIAlertAction = UIAlertAction(title: "OK",
+                                                            style: UIAlertActionStyle.Default,
+                                                            handler:{
+                                                                (action:UIAlertAction!) -> Void in
+                                                                let textFields:Array<UITextField>? =  alert.textFields as Array<UITextField>?
+                                                                if textFields != nil {
+                                                                    for textField:UITextField in textFields! {
+                                                                        //各textにアクセス
+                                                                        if textField.text == ""{
+                                                                            textField.text = "ゲスト"
+                                                                        }
+                                                                        UserDefaultSurpport.userName = textField.text
+                                                                        self.tableView.cellForRowAtIndexPath(indexPath)?.detailTextLabel?.text = textField.text
+                                                                        self.navigationController!.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont(name: "HiraKakuProN-W6", size: 14)!]
+                                                                    }
+                                                                }
+            })
+            alert.addAction(defaultAction)
+            
+            
+            presentViewController(alert, animated: true, completion: nil)
+        }
     }
 }
